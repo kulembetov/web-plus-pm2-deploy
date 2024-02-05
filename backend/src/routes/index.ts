d1bc76555ca30385fae18ba8060d1d4f4a2fc089
Artur Kulembetov <artur_kulembetov@outlook.com>
2024-02-05 23:11:43 +0700

    feat: add '/crash-test' route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -12,6 +12,11 @@ import {
 const router = Router();
 router.post("/signup", validateUserBody, createUser);
 router.post("/signin", validateAuthentication, login);
+router.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
